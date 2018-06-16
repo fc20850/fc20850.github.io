624f4b37f6e0cd037cb33d650a1530b4027ee2ea
Fang Chen <fc20850@gmail.com>
2018-06-16 14:41:27 -0400

    test

--- gpod.js
+++ gpod.js
@@ -1,24 +1,24 @@
 var app = angular.module('gpodApp', []);
 
 app.controller('gpodCtrl', function($scope, $http) {
-	
+
   /*var enc = $.base64.encode("this is a test");
   alert("e=" + enc);
   var dec = $.base64.decode("dGhpcyBpcyBhIHRlc3Q=");
   alert("d=" + dec);*/
-  
-//  var authdata = $.base64.encode('fc20850:Deerbrook1');
-//  $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+
+  var authdata = $.base64.encode('fc20850:Deerbrook1');
+  $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
    $http.post('https://gpodder.net/api/2/auth/fc20850/login.json', {})
        .then(function (response) {
            alert(response);
        });
-	   
+
 	/*
   $http.get("https://gpodder.net/api/2/tags/2.json")
   .then(function(response) {
       $scope.tags = response.data;
 	  alert("s=" + $scope.tags.length);
   });
-		*/	
+		*/
 });
